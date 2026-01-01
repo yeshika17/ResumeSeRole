017df3abbf70ad1a41862c4b9957b7c95668462f
Yeshika <yeshikagola139@example.com>
2026-01-01 17:39:32 +0530

    Fix express catch-all route and deployment setup

--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,40 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import cors from "cors";
 import analyzeRoute from "./routes/analyze.routes.js";
 import jobRoute from "./routes/job.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Server is running ✅");
-});
+
 
 app.use("/api/jobs", jobRoute);
 app.use("/api/analyze", analyzeRoute);
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
